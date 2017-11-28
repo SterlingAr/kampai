1db7293fad0a45b77981798962e403d03e06d3ae
Your Name <you@example.com>
2017-11-28 12:35:48 +0100

    Start nodos

---
+++ database/migrations/2017_11_28_100356_create_nodes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNodes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nodes', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->bigInteger('node')->unique();
+            $table->text('keywords');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nodes');
+
+    }
+}
